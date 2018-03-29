4a5490c33e4bf030ba232532812dc9dc8c88bd1a
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-03-29 14:57:22 -0400

    Add arraywriter.js

---
+++ arraywriter.js
@@ -0,0 +1,52 @@
+/**
+ * Class that writes the contents of an array to a webpage.
+ * @author Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
+ */
+
+class ArrayWriter {
+    
+    /**
+     * Creates an instance of ArrayPrinter.
+     * @constructor
+     * @param {Array} list The desired array to fill the ArrayPrinter with. 
+     */
+    constructor(list) {
+        this._list = list;
+    }
+    
+    /** Writes the contents of _list to the document in a paragraph tag. */
+    writeArray() {
+        this._list.forEach(function(element) {
+            this.writeArray("<p>","</p>")
+        });           
+    }
+    
+    
+    /**
+     * Writes the contents of _list to the document between the entered tags.
+     * @param {String} startTag The desired start tag to write.
+     * @param {String} endTag   The desired end tag to write.
+     */
+    writeArray(startTag, endTag) {
+        this._list.forEach(function(element) {
+            document.write(startTag + element + endTag);
+        }); 
+    }
+    
+    /**
+     * Getter
+     * @returns {Array} The current array of this instance of ArrayWriter.
+     */
+    get list() {
+        return this._list;
+    }
+    
+    /**
+     * Setter
+     * @param {Array} list The desired array to fill the ArrayPrinter with. 
+     */
+    set list(list) {
+        this._list = list;
+    }
+    
+}
